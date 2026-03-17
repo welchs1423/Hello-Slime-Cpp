321abe9bd9938c9e3e338ec556c9e77aed9f48c7
welchs1423 <welchs1423@naver.com>
2026-03-18 01:12:58 +0900

    fix: Colors.h 파일에 누락된 BLUE 색상 복구

--- include/Colors.h
+++ include/Colors.h
@@ -7,7 +7,8 @@ const std::string RESET = "\033[0m";
 const std::string RED = "\033[31m";
 const std::string GREEN = "\033[32m";
 const std::string YELLOW = "\033[33m";
-const std::string CYAN = "\033[36m";
+const std::string BLUE = "\033[34m";
 const std::string MAGENTA = "\033[35m";
+const std::string CYAN = "\033[36m";
 
 #endif
