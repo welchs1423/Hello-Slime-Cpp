c690445e602fa594f9c4cfc46c04b408c3a21de2
welchs1423 <welchs1423@naver.com>
2026-03-18 01:10:37 +0900

    fix: Colors.h 파일에 MAGENTA 색상 정의 추가

--- include/Colors.h
+++ include/Colors.h
@@ -1,11 +1,13 @@
 #ifndef COLORS_H
 #define COLORS_H
 
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define CYAN    "\033[36m"
+#include <string>
+
+const std::string RESET = "\033[0m";
+const std::string RED = "\033[31m";
+const std::string GREEN = "\033[32m";
+const std::string YELLOW = "\033[33m";
+const std::string CYAN = "\033[36m";
+const std::string MAGENTA = "\033[35m";
 
 #endif
