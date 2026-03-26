dc17220c2b2a40a6dc9481a8207f1bd8ecb6bc25
welchs1423 <welchs1423@naver.com>
2026-03-27 01:03:11 +0900

    feat: 몬스터 스폰 로직 외부 CSV 데이터 연동 (Data-Driven 아키텍처 구현)

--- src/MonsterFactory.cpp
+++ src/MonsterFactory.cpp
@@ -1,26 +1,76 @@
 #include "../include/MonsterFactory.h"
-#include "../include/Slime.h"
-#include "../include/Goblin.h"
-#include "../include/KingSlime.h"
-#include "../include/Orc.h"
-#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iostream>
 
-Monster* MonsterFactory::spawnMonster(int floor, int playerLevel) {
-    // 1. 보스 생성 로직 (5층 단위)
-    if (floor % 5 == 0) {
-        return new KingSlime(playerLevel); // 이제 평범한 몬스터가 아니라 진짜 보스 객체를 반환
+using namespace std;
+
+Monster *MonsterFactory::spawnMonster(int floor, int playerLevel)
+{
+    string mName = "알 수 없는 몬스터";
+    int mHp = 30 + (floor * 5);
+    int mAtk = 5 + floor;
+
+    ifstream file("monsters.csv");
+    if (file.is_open())
+    {
+        string line;
+        bool isFirstLine = true;
+
+        while (getline(file, line))
+        {
+            if (isFirstLine)
+            {
+                isFirstLine = false;
+                continue;
+            }
+
+            stringstream ss(line);
+            string token;
+            vector<string> data;
+
+            while (getline(ss, token, ','))
+            {
+                data.push_back(token);
+            }
+
+            if (data.size() >= 4)
+            {
+                int targetFloor = stoi(data[0]);
+                if (floor <= targetFloor)
+                {
+                    mName = data[1];
+                    mHp = stoi(data[2]) + (floor * 3);
+                    mAtk = stoi(data[3]) + (floor / 2);
+                    break;
+                }
+            }
+        }
+        file.close();
+    }
+    else
+    {
+        if (floor >= 10)
+        {
+            mName = "오크 족장";
+            mHp = 150;
+            mAtk = 20;
+        }
+        else if (floor >= 5)
+        {
+            mName = "고블린 전사";
+            mHp = 80;
+            mAtk = 10;
+        }
+        else
+        {
+            mName = "야생의 슬라임";
+            mHp = 30;
+            mAtk = 5;
+        }
     }
 
-    // 2. 일반 몬스터 배정 로직 (Battle에서 이사 옴!)
-    if (floor >= 7) {
-        if (rand() % 2 == 0) return new Orc(playerLevel);
-        return new Goblin(playerLevel);
-    } 
-    
-    if (floor >= 3) {
-        if (rand() % 2 == 0) return new Goblin(playerLevel);
-        return new Slime(playerLevel);
-    }
-
-    return new Slime(playerLevel);
+    return new Monster(mName, mHp, mAtk);
 }
